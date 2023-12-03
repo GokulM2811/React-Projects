db6deefb6ac80b67c5d55c783b3f5a6caa6a56fb
gokul.mt <gokul.mt@zohocorp.com>
2023-12-03 16:34:44 +0530

    handling event in a seperate function.

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,17 +1,16 @@
+import { MouseEvent } from "react";
+
 function ListGroup() {
   let items = ["Chennai", "Coimbatore", "Maduri", "Thirunelveli"];
   //   items = [];
+  const handleClick = (event: MouseEvent) => console.log(event);
   return (
     <>
       <h1>List</h1>
       {items.length === 0 && <p>No Items found</p>}
       <ul className="list-group">
         {items.map((item, index) => (
-          <li
-            onClick={(event) => console.log(item + "-" + index)}
-            key={item}
-            className="list-group-item"
-          >
+          <li onClick={handleClick} key={item} className="list-group-item">
             {item}
           </li>
         ))}
